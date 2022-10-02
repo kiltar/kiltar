644db88e69ea33bb3e3e5032e895d9ae062f5901
kiltarmeghwar <kiltar2020@namal.edu.pk>
2022-10-02 01:35:06 -0700

    second commit

---
+++ cal02.py
@@ -0,0 +1,12 @@
+# calculator in python(v0.2)
+
+def add(num1, num2) :
+     return num1 + num2
+
+def sub(num1, num2) :
+     return num - num2
+
+# main
+res = add (res)
+print(res)
+print(sub(5,3)
